a9742ed6c8edf72a4d89a607fc009e12d9c864fa
Thomas BRISBOUT <thomas.brisbout@gmail.com>
2021-12-01 21:31:09 +0100

    solve day1 part2

--- day1/main.go
+++ day1/main.go
@@ -10,11 +10,7 @@ func solvePart1(data string) int {
 	count := 0
 	arr := strings.Split(strings.TrimSpace(data), "\n")
 
-	for i, _ := range arr {
-		if i == 0 {
-			continue
-		}
-
+	for i := 1; i < len(arr); i += 1 {
 		a, _ := strconv.Atoi(arr[i])
 		b, _ := strconv.Atoi(arr[i-1])
 
@@ -26,6 +22,23 @@ func solvePart1(data string) int {
 	return count
 }
 
+func solvePart2(data string) int {
+	count := 0
+	arr := strings.Split(strings.TrimSpace(data), "\n")
+
+	for i := 3; i < len(arr); i += 1 {
+		a, _ := strconv.Atoi(arr[i])
+		b, _ := strconv.Atoi(arr[i-3])
+
+		if a > b {
+			count++
+		}
+	}
+
+	return count
+}
+
 func main() {
 	fmt.Printf("solution for Part 1: %v\n", solvePart1(input))
+	fmt.Printf("solution for Part 2: %v\n", solvePart2(input))
 }
--- day1/main_test.go
+++ day1/main_test.go
@@ -2,8 +2,7 @@ package main
 
 import "testing"
 
-func TestSolvePart1(t *testing.T) {
-	testInput := `
+const testInput = `
 199
 200
 208
@@ -16,8 +15,18 @@ func TestSolvePart1(t *testing.T) {
 263
 	`
 
+func TestSolvePart1(t *testing.T) {
 	want := 7
 	got := solvePart1(testInput)
+
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
+	}
+}
+
+func TestSolvePart2(t *testing.T) {
+	want := 5
+	got := solvePart2(testInput)
 
 	if got != want {
 		t.Errorf("got %d want %d", got, want)
