5bd503dc31fb2d3f811020fc4f55a3b520ab1678
Thomas BRISBOUT <thomas.brisbout@gmail.com>
2021-12-02 22:05:01 +0100

    solve day2 part2

--- day2/main.go
+++ day2/main.go
@@ -31,6 +31,29 @@ func solvePart1(data string) int {
 	return h * d
 }
 
+func solvePart2(data string) int {
+	var h, d, a int
+
+	for _, command := range parse(data) {
+		c := strings.Split(command, " ")
+		dir := c[0]
+		n, _ := strconv.Atoi(c[1])
+
+		switch dir {
+		case "forward":
+			h += n
+			d += n * a
+		case "up":
+			a -= n
+		case "down":
+			a += n
+		}
+	}
+
+	return h * d
+}
+
 func main() {
 	fmt.Printf("solution for Part 1: %v\n", solvePart1(input))
+	fmt.Printf("solution for Part 2: %v\n", solvePart2(input))
 }
--- day2/main_test.go
+++ day2/main_test.go
@@ -2,8 +2,7 @@ package main
 
 import "testing"
 
-func TestSolvePart1(t *testing.T) {
-	input := `
+const testInput = `
 forward 5
 down 5
 forward 8
@@ -12,8 +11,18 @@ down 8
 forward 2
 `
 
+func TestSolvePart1(t *testing.T) {
 	want := 150
-	got := solvePart1(input)
+	got := solvePart1(testInput)
+
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
+	}
+}
+
+func TestSolvePart2(t *testing.T) {
+	want := 900
+	got := solvePart2(testInput)
 
 	if got != want {
 		t.Errorf("got %d want %d", got, want)
